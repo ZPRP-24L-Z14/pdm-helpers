d61d981d860cb33ddf842547e0a881b415a648d2
Adam Jeliński <adas.jelinski@gmail.com>
2024-06-01 23:33:56 +0200

    Add temporary folders and argparse

--- src/pdm_helpers/test_runner.py
+++ src/pdm_helpers/test_runner.py
@@ -1,12 +1,11 @@
 from typing import List, Dict
-import os
-import shutil
 import subprocess
 import time
+import tempfile
+import argparse
+from pathlib import Path
 from tqdm import tqdm
 from tabulate import tabulate
-
-work_dir = "./testing_project"  # TODO: use a temp dir
 
 packages_list = [
     "numpy",
@@ -29,20 +28,6 @@ packages_list = [
     "scipy",
 ]
 
-# TODO: remove relative paths
-testing_commands = {
-    "pdm list": ["pdm", "list"],
-    "findpython": ["pdm", "run", "../src/pdm_helpers/findpython.py"],
-    "regular python": ["pdm", "run", "python3", "../src/pdm_helpers/speed_test.py"],
-    "python without site modules": [
-        "pdm",
-        "run",
-        "python3",
-        "-S",
-        "../src/pdm_helpers/speed_test.py",
-    ],
-}
-
 
 def execute_command(command: List[str], cwd="", mute_output=False):
     if mute_output:
@@ -53,12 +38,12 @@ def execute_command(command: List[str], cwd="", mute_output=False):
         subprocess.run(command, cwd=cwd)
 
 
-def run_command(command: List[str], cwd="", repeats=1, mute_output=False):
+def run_command(command: List[str], cwd="", repetitions=1, mute_output=False):
     start_time = time.perf_counter()
-    if repeats == 1:
+    if repetitions == 1:
         execute_command(command, cwd=cwd, mute_output=mute_output)
     else:
-        for i in tqdm(range(repeats)):
+        for i in tqdm(range(repetitions)):
             execute_command(command, cwd=cwd, mute_output=mute_output)
 
     end_time = time.perf_counter()
@@ -66,16 +51,11 @@ def run_command(command: List[str], cwd="", repeats=1, mute_output=False):
     return end_time - start_time
 
 
-def initialize_project():
-    if os.path.isdir(work_dir):
-        shutil.rmtree(work_dir)
-
-    os.makedirs(work_dir)
-
+def initialize_project(work_dir):
     run_command(["pdm", "init", "-n"], cwd=work_dir)
 
 
-def install_packages():
+def install_packages(work_dir):
     run_command(["pdm", "add"] + packages_list, cwd=work_dir)
 
 
@@ -92,8 +72,8 @@ def report_results(
         table.append(
             [
                 test_name,
-                f"{clean_time:.5f}",
-                f"{dirty_time:.5f}",
+                f"{clean_time:.5f}s",
+                f"{dirty_time:.5f}s",
                 f"{percent_difference:.2f}%",
             ]
         )
@@ -105,36 +85,81 @@ def report_results(
     )
 
 
-def test_performance_difference(test_suite, repeats):
-    initialize_project()
+def test_performance_difference(test_suite, repetitions):
+    with tempfile.TemporaryDirectory() as work_dir:
+        initialize_project(work_dir)
 
-    clean_project_times = {}
-    dirty_project_times = {}
+        clean_project_times = {}
+        dirty_project_times = {}
 
-    print("Running tests on clean project...")
-    for test_name, test_command in test_suite.items():
-        print(f"Test {test_name}:")
-        clean_project_times[test_name] = (
-            run_command(test_command, repeats=repeats, cwd=work_dir, mute_output=True)
-            / repeats
-        )
+        print("Running tests on clean project...")
+        for test_name, test_command in test_suite.items():
+            print(f"Test {test_name}:")
+            clean_project_times[test_name] = (
+                run_command(
+                    test_command,
+                    repetitions=repetitions,
+                    cwd=work_dir,
+                    mute_output=True,
+                )
+                / repetitions
+            )
 
-    install_packages()
+        install_packages(work_dir)
 
-    print("Running tests on dirty project...")
-    for test_name, test_command in test_suite.items():
-        print(f"Test {test_name}:")
-        dirty_project_times[test_name] = (
-            run_command(test_command, repeats=repeats, cwd=work_dir, mute_output=True)
-            / repeats
-        )
+        print("Running tests on dirty project...")
+        for test_name, test_command in test_suite.items():
+            print(f"Test {test_name}:")
+            dirty_project_times[test_name] = (
+                run_command(
+                    test_command,
+                    repetitions=repetitions,
+                    cwd=work_dir,
+                    mute_output=True,
+                )
+                / repetitions
+            )
 
-    report_results(clean_project_times, dirty_project_times)
+        report_results(clean_project_times, dirty_project_times)
 
 
 def main():
-    test_performance_difference(testing_commands, repeats=5)
-    # TODO: add argparse for testing options
+    current_path = Path(__file__).parent
+    testing_commands = {
+        "pdm list": ["pdm", "list"],
+        "findpython": [
+            "pdm",
+            "run",
+            (current_path / "../src/pdm_helpers/findpython.py").resolve(),
+        ],
+        "regular python": [
+            "pdm",
+            "run",
+            "python3",
+            (current_path / "../src/pdm_helpers/speed_test.py").resolve(),
+        ],
+        "python without site modules": [
+            "pdm",
+            "run",
+            "python3",
+            "-S",
+            (current_path / "../src/pdm_helpers/speed_test.py").resolve(),
+        ],
+    }
+
+    parser = argparse.ArgumentParser(
+        description="Compare the performance of Python startup without packages and with many packages installed"
+    )
+    parser.add_argument(
+        "--repetitions",
+        type=int,
+        default=1,
+        help="number of test repetitions to run (default 1)",
+    )
+
+    args = parser.parse_args()
+
+    test_performance_difference(testing_commands, repetitions=args.repetitions)
 
 
 if __name__ == "__main__":
